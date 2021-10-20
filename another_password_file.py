5cfa5d4ee9ea53c9d3bac65ec3f736baeb4adb9e
Travis Lloyd <travis@cycode.com>
2021-10-20 14:37:47 -0500

    fixed password issue

--- another_password_file.py
+++ another_password_file.py
@@ -1,5 +1,1 @@
 # More passwords
-my_password = 'new_pa$$word'
-my_specific_password = 'thi$_1s_@_very_$pec1f1c_Tr@v1$_Ll0yd_2'
-my_api_key = '85881850690ff9ac6fc45afc:d0135055c553eff5fb7aa1897c75fe0a87f6ec922aedc4a5eca61ff8fa588cffaea5fe5f'
-twilio_key = 'SKe4d0b1fe08b0611032c14fa6dc699fa2'
